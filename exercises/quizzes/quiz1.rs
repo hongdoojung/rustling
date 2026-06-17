fdcc5e76b8489a20839ebe382f5c8c16edb1a934
hongdoojung <hongdoo.jung@icloud.com>
2026-06-17 21:01:20 +0900

    260617, 03-02

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -16,6 +16,10 @@ fn main() {
     // You can optionally experiment here.
 }
 
+fn calculate_price_of_apples(q: i32) -> i32 {
+    if q > 40 { q * 1 } else { q * 2 }
+}
+
 // Don't change the tests!
 #[cfg(test)]
 mod tests {
